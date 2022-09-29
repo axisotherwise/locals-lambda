0ae9ff004f9d11e2fa0daee69c8bfe54b7607b40
axis <axisotherwise@gmail.com>
2022-09-30 04:48:59 +0900

    Update lambda profile

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
-  const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length - 1];
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg 사용합니다.
-  console.log('name', filename, 'ext', ext);
+exports.handler = async (event, context, done) => {
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = event.Records[0].s3.object.key;
+    const filename = Key.split('/')[Key.split('/').length - 1];
 
-  try {
-    const s3Object = await s3.getObject({ Bucket, Key }).promise(); // 버퍼로 가져오기
-    console.log('original', s3Object.Body.length);
-    const resizedImage = await sharp(s3Object.Body) // 리사이징
-      .resize(200, 200, { fit: 'inside' })
-      .toFormat(requiredFormat)
-      .toBuffer();
-    await s3.putObject({ 
-      Bucket,
-      Key: `comment-resized/${filename}`,
-      Body: resizedImage,
-    }).promise();
-    console.log('put', resizedImage.length);
-    return callback(null, `comment-resized/${filename}`);
-  } catch (error) {
-    console.error(error);
-    return callback(error);
-  }
+    const ext = Key.split('.')[Key.split('.').length - 1];
+    const format = ext === 'jpg' ? 'jpeg' : ext; 
+
+    try {
+        const s3Object = await s3.getObject({ Bucket, Key }).promise(); 
+
+        const resizedImage = await sharp(s3Object.Body) 
+            .resize(150, 150, { fit: 'inside' })
+            .toFormat(format)
+            .toBuffer();
+
+        await s3.putObject(
+            { 
+                Bucket,
+                Key: `lee-resized/${filename}`,
+                Body: resizedImage,
+            }
+        ).promise();
+
+        return done(null, `lee-resized/${filename}`);
+    } catch (error) {
+        return done(error);
+    }
 };
